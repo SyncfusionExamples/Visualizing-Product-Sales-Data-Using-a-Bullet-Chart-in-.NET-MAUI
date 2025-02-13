6dc3e1d4ef1d6fbbbf80aaba5fd07dfe881c34a0
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2025-02-13 19:11:56 +0530

    attach the blog sample

---
+++ BulletChartSample/BulletChartSample/GaugeModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletChartSample
+{
+    public class GaugeModel
+    {
+        public string Category { get; set; }
+
+        public double ObservedValue { get; set; }
+
+        public double TargetValue { get; set; }
+
+        public double LowRange { get; set; }
+
+        public double MidRange { get; set; }
+
+        public double HighRange { get; set; }
+
+        public bool IsAxisVisible { get; set; } = false;
+
+        public GaugeModel(string text, double actual, double target, double low, double mid, double high)
+        {
+            Category = text;
+            ObservedValue = actual;
+            TargetValue = target;
+            LowRange = low;
+            MidRange = mid;
+            HighRange = high; 
+        }
+    }
+}
---
+++ BulletChartSample/BulletChartSample/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulletChartSample
+{
+    public class ViewModel
+    {
+        public ObservableCollection<GaugeModel> BulletChartData { get; set; }
+
+        public ViewModel()
+        {
+            BulletChartData = new ObservableCollection<GaugeModel>()
+            {
+              new GaugeModel("Product A", 190, 220, 150, 180, 210),
+              new GaugeModel("Product B", 175, 180, 130, 160, 190 ),
+              new GaugeModel("Product C", 195, 200, 130, 170, 210),
+              new GaugeModel("Product D", 205, 180, 140, 170, 210),
+              new GaugeModel("Product E", 215, 220, 150, 190, 220),
+              new GaugeModel("Product F", 185, 200, 140, 170, 210),
+              new GaugeModel("Product G", 208, 200, 150, 190, 220){ IsAxisVisible = true }
+            };
+        }
+    }
+}
